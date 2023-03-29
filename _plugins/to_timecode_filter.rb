f9846388d24f8b62af1d7b53f42d63fe33069212
Andrey Satarin <asatarin@users.noreply.github.com>
2023-03-28 18:38:49 -0700

    Update to_timecode_filter.rb

--- _plugins/to_timecode_filter.rb
+++ _plugins/to_timecode_filter.rb
@@ -1,8 +1,8 @@
 module ToTimeCodeFilter
   def to_timecode(input, summary_text = "Click to expand")
-    markdown = Liquid::Template.parse(input).render(@context)
-    html = Jekyll::Renderer.new(@context.registers[:site], {}).convert(markdown)
-    "<span style="color:blue">html</span>"
+#     markdown = Liquid::Template.parse(input).render(@context)
+#     html = Jekyll::Renderer.new(@context.registers[:site], {}).convert(markdown)
+    "<span style="color:blue">input</span>"
   end
 end
 
